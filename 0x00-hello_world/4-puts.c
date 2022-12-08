de3ffba8d5db02faf0119185c93bad968f096151
Mina Garss <minazekry92@gmail.com>
2022-12-08 08:39:27 -0800

    creating 4-puts.c file for task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Writing a function that prints a specific string
+ * Return: An exit code of 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
