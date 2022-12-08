e6b1cbe5c00779e4270611a6cb196ef72b972141
Mina Garss <minazekry92@gmail.com>
2022-12-08 08:46:31 -0800

    creating 5-printf.c file for task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Writing a function that prints a specific string
+ * Return: An exit code of 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
