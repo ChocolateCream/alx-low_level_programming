1645644d8fb556c69d9de53ba89e7761e66ffa66
Mina Garss <minazekry92@gmail.com>
2022-12-09 16:08:13 +0200

    creating 6-print_numberz.c file for task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers from 0 to 9
+ * Return: An exit code of 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
