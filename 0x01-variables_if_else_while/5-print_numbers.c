55e135b224faa477155b97419b289d908313deaa
Mina Garss <minazekry92@gmail.com>
2022-12-09 15:52:34 +0200

    creating 5-print_numbers.c file for task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers from 0 to 9
+ * Return: An exit code of 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
