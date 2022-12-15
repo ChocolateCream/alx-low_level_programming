3ca8a3f5482a8357bdb40e062355a2e35a1de863
Mina Garss <minazekry92@gmail.com>
2022-12-15 22:07:11 +0200

    creating 8-print_square.c file for task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square followed by new line
+ * @size: The size of the square
+ *
+ * Return: Nothing
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
