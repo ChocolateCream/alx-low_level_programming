b778dba3aa1f1fcfd607acbd6409224d958255e3
Mina Garss <minazekry92@gmail.com>
2023-01-09 19:18:37 +0200

    freeing the space if the maloc fails

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,12 +21,20 @@ int **alloc_grid(int width, int height)
 
 	arr = malloc(sizeof(int *) * height);
 	if (arr == NULL)
+	{
+		free(arr);
 		return (NULL);
+	}
+
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
+		{
+			free(arr);
 			return (NULL);
+		}
+
 		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
 	}
