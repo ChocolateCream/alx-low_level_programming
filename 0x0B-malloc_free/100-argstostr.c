b90106500a3394c4c18af2eb2f712decc0e2ec88
Mina Garss <minazekry92@gmail.com>
2023-01-09 23:13:57 +0200

    task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "_strlen.c"
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * argstostr - Concats the arguments passed to the function and adds \\n
+ * @ac: The number of arguments passed
+ * @av: The arguments passed
+ *
+ * Return: A pointer to the concatenated string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *arr;
+	int i;
+	int j;
+	int k;
+	int len;
+
+	if ((ac == 0) | (*av == NULL))
+		return (NULL);
+
+	len = 0;
+	for (i = 0; i < ac; i++)
+		len += _strlen(av[i]);
+
+	len = (sizeof(char) + 1) * len + 1;
+	arr = malloc(len);
+
+	if (arr == NULL)
+		return (NULL);
+
+	i = 0;
+	for (j = 0; j < ac; j++)
+	{
+		for (k = 0; k < _strlen(av[j]); k++)
+		{
+			arr[i + k] = av[j][k];
+		}
+		arr[i + k] = '\n';
+		i += k + 1;
+	}
+
+	arr[len - 1] = '\0';
+	return (arr);
+}
