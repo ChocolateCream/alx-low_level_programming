eb5b08be820511425a5497c64663c3b247e391e4
Mina Garss <minazekry92@gmail.com>
2023-01-09 16:20:53 +0200

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "_strlen.c"
+#include "main.h"
+
+/**
+ * str_concat - Creates an array that concats 2 strings
+ * @s1: The first string that will be concatenated
+ * @s2: The second string that will be concatenated
+ *
+ * Return: A pointer to the created array
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *arr;
+	int len_s1;
+	int len_s2;
+	int i;
+	int j;
+
+	len_s1 = _strlen(s1);
+	len_s2 = _strlen(s2);
+
+	arr = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < len_s1; i++)
+		arr[i] = s1[i];
+
+	for (j = 0; j < len_s2; j++)
+		arr[i + j] = s2[j];
+
+	return (arr);
+}
