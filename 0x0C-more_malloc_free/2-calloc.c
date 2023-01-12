05330b84944fa8bb42838ddbfcffa8c70708019d
Mina Garss <minazekry92@gmail.com>
2023-01-12 20:41:07 +0200

    initializing the allocated memory with 0s

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * _calloc - Allocates memory
@@ -18,6 +19,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(nmemb * size);
 	if (ptr == (NULL))
 		return (NULL);
+
+	memset(ptr, 0, nmemb * size);
 
 	return (ptr);
 }
