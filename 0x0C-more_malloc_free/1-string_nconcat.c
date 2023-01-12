2bfaf7c17604eeeabf9171afdc7fc0c602c6dbe8
Mina Garss <minazekry92@gmail.com>
2023-01-12 19:10:17 +0200

    task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: The first string
+ * @s2: The second string
+ * @n: The number of chars in the second string to concat
+ *
+ * Return: A pointer to the new memory spaces with the concatenated strings
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *arr;
+	char *s;
+	unsigned int len;
+	unsigned int i;
+	unsigned int j;
+
+	if (s2 == NULL)
+	{
+		s = "";
+		len = 0;
+	}
+
+	else if (strlen(s2) < n)
+	{
+		s = s2;
+		len = strlen(s2);
+	}
+
+	else
+	{
+		s = s2;
+		len = n;
+	}
+
+	arr = malloc(sizeof(char) * (strlen(s1) + len + 1));
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < strlen(s1); i++)
+		arr[i] = s1[i];
+
+	for (j = 0; j < len; j++)
+		arr[i + j] = s[j];
+
+	return (arr);
+}
