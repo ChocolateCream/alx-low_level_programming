4cca9de4d1a71e35b6f9a0968256762fe5b5f0ab
Mina Garss <minazekry92@gmail.com>
2023-01-18 12:35:27 +0200

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name - prints a name
+ * @name: a string to the name of the person
+ * @f: a pointer to the function that prints the name
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
