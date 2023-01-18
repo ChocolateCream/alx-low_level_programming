a43d57e63522be464cccb4e6a886a4383a08fb29
Mina Garss <minazekry92@gmail.com>
2023-01-18 12:53:55 +0200

    task 1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef FUNC_POINTERS_H
 #define FUNC_POINTERS_H
+#include <stdlib.h>
 
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
