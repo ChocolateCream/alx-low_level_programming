cecd941c76a6ab391991f0aa9542b8b41ec0e7ae
Mina Garss <minazekry92@gmail.com>
2023-01-18 12:38:41 +0200

    taking care of NULL functions in task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 /**
  * print_name - prints a name
  * @name: a string to the name of the person
@@ -8,5 +10,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != NULL)
+		f(name);
 }
