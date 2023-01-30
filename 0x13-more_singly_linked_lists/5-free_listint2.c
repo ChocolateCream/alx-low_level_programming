e472d138839ef508108f25ac92a55925bb7ea5c5
Mina Garss <minazekry92@gmail.com>
2023-01-30 18:17:54 +0200

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+
+/**
+ * free_listint2 - Frees the memory occupied by the list.
+ * @head: Pointer to the head pointer.
+ *
+ * Return: Nothing.
+ */
+
+void free_listint2(listint_t **head)
+
+{
+	listint_t *next_node;
+
+	while (*head)
+	{
+		next_node = (*head)->next;
+		free(*head);
+		*head = next_node;
+	}
+}
