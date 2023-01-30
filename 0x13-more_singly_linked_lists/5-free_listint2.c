7b0b83b0cc6a3d6e87897f81e131b94e7fc036f1
Mina Garss <minazekry92@gmail.com>
2023-01-30 18:27:55 +0200

    task 5, considering if there is no pointer to the head pointer

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,11 +13,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *next_node;
 
+	if (!(head))
+		return;
+
 	while (*head)
 	{
 		next_node = (*head)->next;
 		free(*head);
 		*head = next_node;
 	}
-	*head = NULL;
 }
