5c4f4099636bcd1d998578b662266c419915b4cf
Mina Garss <minazekry92@gmail.com>
2023-01-30 18:41:12 +0200

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+
+/**
+ * pop_listint - Deletes a specific node.
+ * @head: Pointer to the head pointer.
+ *
+ * Return: The value of n of the deleted node.
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *new_head_node;
+	int node_n;
+
+	if (!(head))
+		return (0);
+
+	node_n = (*head)->n;
+	new_head_node = (*head)->next;
+	free(*head);
+	*head = new_head_node;
+	return (node_n);
+}
