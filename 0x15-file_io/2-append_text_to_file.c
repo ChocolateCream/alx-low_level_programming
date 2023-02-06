fb02c2bd8f2d09ff90afa13cae6652b9df61fc7b
Mina Garss <minazekry92@gmail.com>
2023-02-06 22:33:08 +0200

    changing the mode to write only

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	file_desc = open(filename, O_RDWR | O_APPEND);
+	file_desc = open(filename, O_WRONLY | O_APPEND);
 	if (file_desc < 0)
 		return (-1);
 
